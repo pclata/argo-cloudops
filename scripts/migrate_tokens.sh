f74f71e27b4314e75f983844e31a2a8d4316105e
pclata <1316831+pclata@users.noreply.github.com>
2022-07-05 16:15:40 -0700

    initial commit of migrate script

--- scripts/migrate_tokens.sh
+++ scripts/migrate_tokens.sh
@@ -1,26 +1,44 @@
-#!/bin/bash -e
+#!/bin/bash
 
+set -e
+db_host=$PGHOST
+db_port=$PGPORT
+db_name=$PGDATABASE
 
-vaultToken="1234"
-vaultHost="https://url:8200/"
+vault_token=$VAULT_TOKEN
+vault_host=$VAULT_HOST
 
-# call vault for projects/approles
-appRoleResponse=$(curl \
-	--header "X-Vault-Token: $vaultToken" \
+# get list of projects/approles
+approle_response=$(curl -s\
+	--header "X-Vault-Token: $vault_token" \
 	--request LIST \
-	"$vaultHost/v1/auth/approle/role" \
+	"$vault_host/v1/auth/approle/role" \
 	| jq -r '.data.keys')
 
-#for appRole in $(echo "$appRoleResponse" | jq -r '.[]); do
-#done
+for approle in $(echo "$approle_response" | jq -r '.[]'); do
+  # get list of secret id accessors for each approle/project
+  accessors=($(curl -s\
+    --header "X-Vault-Token: $vault_token" \
+    --request LIST \
+    "$vault_host/v1/auth/approle/role/$approle/secret-id" \
+    | jq -r '.data.keys' | tr -d '[]," '))
 
-# call vault for secret id accessor for each approle/project
-# if multiple, triage
-#secretIdAccessors=$(curl \
-#--header "X-Vault-Toke: $vaultToken" \
-#--request LIST \
-#"$vaultHost/v1/auth/approle/role/something/secret-id")
+  # if multiple secret accessors exist, print approle for manual triage.
+	# manually, find which secret belongs to an accessor, and save accessor ID to tokens DB.
+  if [ "${#accessors[@]}" -gt 1 ]; then
+		echo "WARN: Multiple accessors exist for AppRole: $approle"
+		continue
+  fi
 
+  if [ "${#accessors[@]}" -eq 0 ]; then
+		echo "WARN: Accessors do not exist for AppRole: $approle"
+		continue
+  fi
 
+	set +e
+	echo "ERROR: Inserting accessor into tokens table for AppRole: $approle"
+	psql -h $db_host -p $db_port -w -d $db_name -c "INSERT INTO tokens(token_id, created_at, project) VALUES ('${accessors[0]}', current_timestamp, '$approle')"
+	set -e
+done
 
-# insert into tokens DB
+echo "Script complete"
