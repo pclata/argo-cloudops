7d1aa26fab03a6424ad8706d348d33c1085c4e0d
pclata <1316831+pclata@users.noreply.github.com>
2022-07-06 11:42:54 -0700

    updates

--- scripts/migrate_tokens.sh
+++ scripts/migrate_tokens.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-set -e
+set -eu
+: "${PGHOST?PGHOST must be set}"
+: "${PGPORT?PGPORT must be set}"
+: "${PGDATABASE?PGDATABASE must be set}"
+: "${PGUSER?PGUSER must be set}"
+: "${VAULT_HOST?VAULT_HOST must be set}"
+: "${VAULT_TOKEN?VAULT_TOKEN must be set}"
+
 vault_token=$VAULT_TOKEN
 vault_host=$VAULT_HOST
 
@@ -12,15 +19,14 @@ approle_response=$(curl -s\
 	| jq -r '.data.keys')
 
 for approle in $(echo "$approle_response" | jq -r '.[]'); do
-  # get list of secret id accessors for each approle/project
+  # get list of accessors for each approle/project
   accessors=($(curl -s\
     --header "X-Vault-Token: $vault_token" \
     --request LIST \
     "$vault_host/v1/auth/approle/role/$approle/secret-id" \
     | jq -r '.data.keys' | tr -d '[]," '))
 
-  # if multiple secret accessors exist, print approle for manual triage.
-	# manually, find which secret belongs to an accessor, and save accessor ID to tokens DB.
+  # if multiple accessors exist, print approle for manual triage.
   if [ "${#accessors[@]}" -gt 1 ]; then
 		echo "ERROR: Multiple accessors exist for AppRole: $approle"
 		continue
@@ -31,26 +37,21 @@ for approle in $(echo "$approle_response" | jq -r '.[]'); do
 		continue
   fi
 
-
-	#TODO: won't work for internal	
-	approle_prefix="argo-cloudops-projects-"
-	project=${approle#$approle_prefix}
+	# if any modifications are needed to the project name for the DB, it should happen here
+	project=$approle
 
 	exists=$(psql -h $PGHOST -p $PGPORT -d $PGDATABASE -U $PGUSER -w -t -c "SELECT EXISTS(SELECT project from projects where project='$project')")
 	if [ $exists == "f" ]; then
-		# TODO: figure out why project doesn't exist in DB
-		echo "ERROR: Project does not exist in table: $approle"
+		echo "ERROR: Project does not exist in table: $project"
+		continue
 	fi
 
-
 	set +e
-	#echo "Inserting accessor ID into tokens table for AppRole: $approle"
+	echo "Inserting accessor ID into Tokens table for AppRole: $approle"
 	psql -h $PGHOST -p $PGPORT -d $PGDATABASE -U $PGUSER -w <<'SQL' | ...
 	  INSERT INTO tokens(token_id, created_at, project)
 		VALUES ('${accessors[0]}', current_timestamp, '$approle')
 SQL
-	
-	# TODO: catch error
 	set -e
 done
 
