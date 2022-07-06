5312fd82f7a6e3b69a6ffe929618c83a094f9d7b
pclata <1316831+pclata@users.noreply.github.com>
2022-07-05 17:15:32 -0700

    cleanup

--- scripts/migrate_tokens.sh
+++ scripts/migrate_tokens.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 set -e
-db_host=$PGHOST
-db_port=$PGPORT
-db_name=$PGDATABASE
-
 vault_token=$VAULT_TOKEN
 vault_host=$VAULT_HOST
 
@@ -26,18 +22,35 @@ for approle in $(echo "$approle_response" | jq -r '.[]'); do
   # if multiple secret accessors exist, print approle for manual triage.
 	# manually, find which secret belongs to an accessor, and save accessor ID to tokens DB.
   if [ "${#accessors[@]}" -gt 1 ]; then
-		echo "WARN: Multiple accessors exist for AppRole: $approle"
+		echo "ERROR: Multiple accessors exist for AppRole: $approle"
 		continue
   fi
 
   if [ "${#accessors[@]}" -eq 0 ]; then
-		echo "WARN: Accessors do not exist for AppRole: $approle"
+		echo "ERROR: Accessors do not exist for AppRole: $approle"
 		continue
   fi
 
+
+	#TODO: won't work for internal	
+	approle_prefix="argo-cloudops-projects-"
+	project=${approle#$approle_prefix}
+
+	exists=$(psql -h $PGHOST -p $PGPORT -d $PGDATABASE -U $PGUSER -w -t -c "SELECT EXISTS(SELECT project from projects where project='$project')")
+	if [ $exists == "f" ]; then
+		# TODO: figure out why project doesn't exist in DB
+		echo "ERROR: Project does not exist in table: $approle"
+	fi
+
+
 	set +e
-	echo "ERROR: Inserting accessor into tokens table for AppRole: $approle"
-	psql -h $db_host -p $db_port -w -d $db_name -c "INSERT INTO tokens(token_id, created_at, project) VALUES ('${accessors[0]}', current_timestamp, '$approle')"
+	#echo "Inserting accessor ID into tokens table for AppRole: $approle"
+	psql -h $PGHOST -p $PGPORT -d $PGDATABASE -U $PGUSER -w <<'SQL' | ...
+	  INSERT INTO tokens(token_id, created_at, project)
+		VALUES ('${accessors[0]}', current_timestamp, '$approle')
+SQL
+	
+	# TODO: catch error
 	set -e
 done
 
